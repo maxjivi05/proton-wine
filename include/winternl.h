1c838772e0bc182ea950a6dba7d8ffb646f27e27
maxjivi05 <maxjividen@gmail.com>
2026-07-03 13:13:00 -0400

    ntdll: Support loading a unixlib by name (MemoryWineLoadUnixLibByName)

--- include/winternl.h
+++ include/winternl.h
@@ -2465,6 +2465,9 @@ typedef enum _MEMORY_INFORMATION_CLASS {
 #ifdef __WINESRC__
     MemoryWineUnixFuncs = 1000,
     MemoryWineUnixWow64Funcs,
+    MemoryWineLoadUnixLibByName = 1002,
+    MemoryWineLoadUnixLibByNameWow64,
+    MemoryWineUnloadUnixLib,
 #endif
     MemoryFexStatsShm = 2000,
 } MEMORY_INFORMATION_CLASS;
